01bf4f019b46fb800fd5ab6a7f06c7b5f0ede47e
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-08-03 11:50:45 +0300

    Added building & adding-to-path script to all linux, windows-user and windows-system

--- build/build-user.ps1
+++ build/build-user.ps1
@@ -1,3 +1,5 @@
+Set-Location "$PSScriptRoot\.."
+
 cargo build --release
 
 $targetDir = "$env:LOCALAPPDATA\R-touch\bin"
@@ -5,9 +7,10 @@ New-Item -ItemType Directory -Force -Path $targetDir
 
 Move-Item -Force -Path ".\target\release\rtouch.exe" "$targetDir\rtouch.exe"
 
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$targetDir", "User")
+$userPath = [Environment]::GetEnvironmentVariable("Path", "User")
+if ($userPath -notlike "*$targetDir*") {
+    [Environment]::SetEnvironmentVariable("Path", $userPath + ";$targetDir", "User")
+}
 $env:Path += ";$targetDir"
 
-# Test
-reset
 rtouch -V
