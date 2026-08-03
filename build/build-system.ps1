c553360ee3362f768efefdc832b5630be7bd124d
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-08-03 12:09:08 +0300

    FIX: Fixed a syntax bug prevented from running

--- build/build-system.ps1
+++ build/build-system.ps1
@@ -1,6 +1,6 @@
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -WorkingDirectory "`"$scriptDir`"" -Verb RunAs
+    Start-Process powershell.exe -ArgumentList "-NoProfile", "-ExecutionPolicy", "Bypass", "-File", "`"$PSCommandPath`"" -WorkingDirectory $scriptDir -Verb RunAs
     exit
 }
 
