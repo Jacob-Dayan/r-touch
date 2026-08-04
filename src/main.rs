4a563e309aaaed5706debb935fbee956730826f0
rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-08-04 16:17:57 +0300

    Bumped a version

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::{
 #[derive(Parser, Debug)]
 #[command(
     name = "R-touch",
-    version = "1.1.0, Latest until <date-of-new-version> ", // I'll put a date here when bumping version
+    version = "1.2.0, Latest until <date-of-new-version> ", // I'll put a date here when bumping version
     about = "A custom touch implementation in Rust"
 )]
 struct Cli {
