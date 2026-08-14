58070cf16692cbdf7cc6888969131bbdd50a21f7
Jacob Dayan <shahar.dayan.shaked@gmail.com>
2026-08-14 20:42:56 +0300

    Update version number to 1.2.1

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::{
 #[derive(Parser, Debug)]
 #[command(
     name = "R-touch",
-    version = "1.2.0, Latest until 14th of August, 2026",
+    version = "1.2.1, Latest until 14th of August, 2026",
     about = "A custom touch implementation in Rust"
 )]
 struct Cli {
